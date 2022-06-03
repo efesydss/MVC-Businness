97334d807833a08ad153b0c659b665d4201672f2
efesydss <40852666+efesydss@users.noreply.github.com>
2022-06-03 21:36:54 +0300

    __Layout ve RenderBody yapısı giriş

--- StWebLibrary/StWebLibrary/Controllers/HomeController.cs
+++ StWebLibrary/StWebLibrary/Controllers/HomeController.cs
@@ -13,5 +13,14 @@ namespace StWebLibrary.Controllers
         {
             return View();
         }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
     }
 }
